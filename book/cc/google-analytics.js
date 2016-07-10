6c1288fe3801a9d8ee4538ff860bab9fd9183172
Gregor Anzelj <gregor.anzelj@gmail.com>
2016-07-10 23:25:31 +0200

    Interaktivni e-ucbenik v2.05

---
+++ book/cc/google-analytics.js
@@ -0,0 +1,11 @@
+if (jQuery.cookie('cc_cookie_accept') == "cc_cookie_accept") {
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-80227128-1']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document. getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+}
